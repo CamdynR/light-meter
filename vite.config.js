aa5c3e5bbe5f8d7389943bcf63f429ae8de583f2
Camdyn Rasque <rasque.camdyn@gmail.com>
2024-02-19 15:25:12 -0800

    Added simple build process with vite

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+// vite.config.js
+import { resolve } from 'path';
+import { defineConfig } from 'vite'
+import { ViteMinifyPlugin } from 'vite-plugin-minify' 
+
+export default defineConfig({
+  root: resolve(__dirname, 'src'),
+  build: {
+    outDir: resolve(__dirname, 'dist'),
+    rollupOptions: {
+      input: {
+        file: resolve(__dirname, 'src/index.html'),
+      }
+    }
+  },
+  plugins: [
+    ViteMinifyPlugin({}),
+  ]
+})
